c78e0b65bc9f4bb14857d72d94cb0e37601a4cf1
YakauKuranets <124173268+YakauKuranets@users.noreply.github.com>
2026-02-22 01:36:50 +0300

    Delete deploy directory

--- deploy/entrypoint_worker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Worker для scheduler'ов (Duty/Tracker)
-# Важно: миграции выполняем в web-контейнере, чтобы не было гонок.
-
-echo "[worker] starting scheduler worker ..."
-exec python worker.py
